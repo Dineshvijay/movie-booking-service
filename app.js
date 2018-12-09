72a9d07c8e79783de641366cb26f935278878f1e
dineshkumar_v3 <dineshkumar_v3@yahoo.com>
2018-12-09 15:23:11 +0530

    changed default port

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const winston = require('winston')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
-
-
 
 app.listen(PORT, () => {
     winston.info(`listening to port ${PORT}`)
